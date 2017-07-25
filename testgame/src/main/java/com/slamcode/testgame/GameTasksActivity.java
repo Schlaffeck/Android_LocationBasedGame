09cbaba6bf9f5cc3515542acdb3b8bf433f3f6e4
smoriak <smoriak@softserveinc.com>
2017-07-25 15:04:33 +0200

    test game - game tasks list activity

---
+++ testgame/src/main/java/com/slamcode/testgame/GameTasksActivity.java
@@ -0,0 +1,13 @@
+package com.slamcode.testgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameTasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_tasks);
+    }
+}
