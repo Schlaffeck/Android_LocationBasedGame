bd2baf954e0daef8195b357944ad757691783a9e
smoriak <smoriak@softserveinc.com>
2017-07-26 10:39:24 +0200

    test game - simple list of tasks activity

--- testgame/src/main/java/com/slamcode/testgame/GameTasksActivity.java
+++ testgame/src/main/java/com/slamcode/testgame/GameTasksActivity.java
@@ -2,6 +2,15 @@ package com.slamcode.testgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.slamcode.locationbasedgamelayout.view.GameTaskContentSimpleLayoutProvider;
+import com.slamcode.locationbasedgamelayout.view.binding.BindableTasksListRecyclerViewAdapter;
+import com.slamcode.locationbasedgamelib.model.GameTaskData;
+import com.slamcode.locationbasedgamelib.model.builder.GameTaskBuilder;
+
+import java.util.Arrays;
 
 public class GameTasksActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class GameTasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_tasks);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.setupTasksList();
+    }
+
+    private void setupTasksList()
+    {
+        GameTaskData[] data = new GameTaskData[]
+                {
+                        new GameTaskBuilder().withTitle("Task 1").withTextElement("Test text message").getTask(),
+                        new GameTaskBuilder().withTitle("Task 2").withTextElement("Test text message number 2\nWith new line").getTask(),
+                        new GameTaskBuilder().withTitle("Task 3").withTextElement("Test text message number 2\nWith new line\nAnd input")
+                                .withTextInputComparisonElement("Check", "test", "game").getTask(),
+                };
+        RecyclerView recyclerView = (RecyclerView) this.findViewById(R.id.testGame_tasksList_recyclerView);
+        recyclerView.setAdapter(new BindableTasksListRecyclerViewAdapter(Arrays.asList(data), new GameTaskContentSimpleLayoutProvider()));
     }
 }
