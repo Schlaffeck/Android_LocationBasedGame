d2d512a81c1c00a692cbcc25b99ba3318107ae8c
smoriak <smoriak@softserveinc.com>
2017-08-07 11:17:13 +0200

    layout updating with usage of image view for task status

--- locationbasedgamelib.layout/src/main/java/com/slamcode/locationbasedgamelayout/view/binding/Bindings.java
+++ locationbasedgamelib.layout/src/main/java/com/slamcode/locationbasedgamelayout/view/binding/Bindings.java
@@ -1,12 +1,17 @@
 package com.slamcode.locationbasedgamelayout.view.binding;
 
 import android.databinding.BindingAdapter;
+import android.graphics.PorterDuff;
 import android.support.v7.widget.RecyclerView;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 import com.slamcode.locationbasedgamelayout.BR;
+import com.slamcode.locationbasedgamelayout.R;
 import com.slamcode.locationbasedgamelayout.view.GameTaskContentSimpleLayoutProvider;
 import com.slamcode.locationbasedgamelib.model.GameTaskContent;
 import com.slamcode.locationbasedgamelib.model.GameTaskData;
+import com.slamcode.locationbasedgamelib.model.GameTaskStatus;
 
 /**
  * Set of custom bindings for created layouts
@@ -32,5 +37,23 @@ public class Bindings {
                 items,
                 new GameTaskContentSimpleLayoutProvider());
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @BindingAdapter("bind:gameTaskStatusImage")
+    public static void setGameTaskStatusImage(final ImageView imageView, GameTaskData gameTaskData)
+    {
+        gameTaskData.addStatusChangedListener(new GameTaskData.StatusChangedListener() {
+            @Override
+            public void onStatusChanged(GameTaskStatus newStatus) {
+                if(newStatus == GameTaskStatus.Success) {
+                    imageView.setImageResource(R.drawable.ic_done_white_24dp);
+                    imageView.setColorFilter(android.R.color.holo_green_dark);
+                }
+                else if(newStatus == GameTaskStatus.Ongoing)
+                    imageView.setImageResource(R.drawable.ic_arrow_forward_white_24dp);
+                else
+                    imageView.setImageDrawable(null);
+            }
+        });
     }
 }
