8d737f9e970ad3d19ae7930f4ed0f02205d92353
smoriak <smoriak@softserveinc.com>
2017-09-19 14:33:25 +0200

    test game - set of proper tasks

--- testgame/src/main/java/com/slamcode/testgame/data/PersistenceContextContainer.java
+++ testgame/src/main/java/com/slamcode/testgame/data/PersistenceContextContainer.java
@@ -28,64 +28,7 @@ public final class PersistenceContextContainer {
     {
         if(currentContext == null)
         {
-            GameDataBundleProvider<TestGameDataBundle> bundleProvider = new GameDataBundleProvider<TestGameDataBundle>() {
-                @Override
-                public TestGameDataBundle getDefaultBundleInstance() {
-                    TestGameDataBundle result = new TestGameDataBundle();
-                    GameTaskData[] tasksList = new GameTaskData[]
-                            {
-                                    new GameTaskBuilder(1).withTitle("Simple task 1").withTextElement("Test text message")
-                                            .withPictureElement(R.drawable.sromamba).getTask(),
-                                    new GameTaskBuilder(2).withTitle("Multiline task 2").withTextElement("Test text message number 2\nWith new line").getTask(),
-                                    new GameTaskBuilder(3).withTitle("Text Input Task 3").withTextElement("Test text message number 3\nWith new line\nAnd input")
-                                            .withTextInputComparisonElement("Check", "test", "game").getTask(),
-                                    new GameTaskBuilder(4).withTitle("Location input task 4")
-                                            .withTextElement("Some test upper text input")
-                                            .withTextElement("Go to 'Feniks DH' on old market square and try the button")
-                                            .withLocationComparisonElement("Are you there yet?", 51.109651f, 17.033557f, 15f, null).getTask(),
-                                    new GameTaskBuilder(5).withTitle("Audio task 5")
-                                            .withTextElement("Play some sound!")
-                                            .withAudioPlayerElement(R.raw.sound, "Music", applicationContext)
-                                            .getTask(),
-                                    new GameTaskBuilder(6).withTitle("Home location task 4")
-                                            .withTextElement("Go home now")
-                                            .withLocationComparisonElement("Are you home?", 51.070847f, 16.996691f, 15f, null).getTask(),
-                                    new GameTaskBuilder(7).withTitle("Task with tips")
-                                            .withTextElement("Try different answers to see tips.")
-                                            .withTextElement("For example: 'dog', 'cat, 'whatever' other or empty answer")
-                                            .withTextInputComparisonElement("Check", "answer", "1234", "password")
-                                            .withTipForPreviousTextInputElement("Don't know about this one, try something else. Don't give up!")
-                                            .withTipForPreviousTextInputElement("Nope, that's not the case. Keep on trying!")
-                                            .withTipForPreviousTextInputElement("dog", "Dogs are nice, they like to wipe their tail.\nBut that's not the answer.")
-                                            .withTipForPreviousTextInputElement("cat", "Cats are good domestic pets.\nHowever it is not the answer.")
-                                            .withTipForPreviousTextInputElement("", "You did not type anything.")
-                                            .withTipForPreviousTextInputElement(null, "You did not type anything.")
-                                            .withTipForPreviousTextInputElement("whatever", "So do you have enough of this game or what?")
-                                            .getTask(),
-                            };
-                    result.setGameTasks(Arrays.asList(tasksList));
-                    return result;
-                }
-
-                @Override
-                public void updateBundle(TestGameDataBundle dataBundle) {
-                    TestGameDataBundle defaultBundle = getDefaultBundleInstance();
-//                    dataBundle.setGameTasks(defaultBundle.getGameTasks());
-                    if(defaultBundle.getGameTasks().size() > dataBundle.getGameTasks().size()) {
-
-                        Collection<GameTaskData> newTasks = defaultBundle.getGameTasks().subList(
-                                dataBundle.getGameTasks().size(),
-                                defaultBundle.getGameTasks().size());
-
-                        dataBundle.getGameTasks().addAll(newTasks);
-                    }
-                }
-
-                @Override
-                public Class<TestGameDataBundle> getBundleClassType() {
-                    return TestGameDataBundle.class;
-                }
-            };
+            GameDataBundleProvider<TestGameDataBundle> bundleProvider = new TestGameDataBundleProvider(applicationContext);
 
             currentContext = new JsonFilePersistenceContext<>(applicationContext, FILE_NAME, bundleProvider);
 
---
+++ testgame/src/main/java/com/slamcode/testgame/data/TestGameDataBundleProvider.java
@@ -0,0 +1,104 @@
+package com.slamcode.testgame.data;
+
+import android.content.Context;
+
+import com.slamcode.locationbasedgamelib.model.GameTaskData;
+import com.slamcode.locationbasedgamelib.model.builder.GameTaskBuilder;
+import com.slamcode.locationbasedgamelib.persistence.GameDataBundleProvider;
+import com.slamcode.testgame.R;
+
+import java.util.List;
+
+public final class TestGameDataBundleProvider implements GameDataBundleProvider<TestGameDataBundle> {
+
+    private Context context;
+
+    public TestGameDataBundleProvider(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public TestGameDataBundle getDefaultBundleInstance() {
+        TestGameDataBundle defaultBundle = new TestGameDataBundle();
+        List<GameTaskData> gameTasks = defaultBundle.getGameTasks();
+
+        gameTasks.add(createTaskCat());
+        gameTasks.add(createTaskTheatre());
+        gameTasks.add(createTaskKiller());
+        gameTasks.add(createTaskCreature());
+
+        gameTasks.add(createTaskFinal());
+
+        return defaultBundle;
+    }
+
+    @Override
+    public void updateBundle(TestGameDataBundle dataBundle) {
+            dataBundle.setGameTasks(this.getDefaultBundleInstance().getGameTasks());
+    }
+
+    @Override
+    public Class<TestGameDataBundle> getBundleClassType() {
+        return TestGameDataBundle.class;
+    }
+
+    private GameTaskData createTaskCat() {
+        return new GameTaskBuilder(1).withTitle(getString(R.string.game_task_cat_title))
+                .withTextElement(getString(R.string.game_task_cat_content))
+                .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_cat_answers))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_cat_tips_answers), getStringArray(R.array.game_task_cat_tips_messages))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_cat_tips_general_messages))
+                .getTask();
+    }
+
+    private GameTaskData createTaskTheatre()
+    {
+        return new GameTaskBuilder(2).withTitle(getString(R.string.game_task_theatre_title))
+                .withTextElement(getString(R.string.game_task_theatre_content))
+                .withLocationComparisonElement(getString(R.string.game_task_location_input_confirm_button_text),
+                        getFloat(R.dimen.game_task_theatre_location_latitude),
+                        getFloat(R.dimen.game_task_theatre_location_longitude),
+                        getFloat(R.dimen.game_task_theatre_location_acceptance_distance),
+                        null)
+                .getTask();
+    }
+
+    private GameTaskData createTaskKiller() {
+        return new GameTaskBuilder(3).withTitle(getString(R.string.game_task_killer_title))
+                .withTextElement(getString(R.string.game_task_killer_content))
+                .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_killer_answers))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_killer_tips_general_messages))
+                .getTask();
+    }
+
+    private GameTaskData createTaskCreature() {
+        return new GameTaskBuilder(4).withTitle(getString(R.string.game_task_creature_title))
+                .withTextElement(getString(R.string.game_task_creature_content_before_audio))
+                .withAudioPlayerElement(R.raw.creature_2, null, this.context)
+                .withTextElement(getString(R.string.game_task_creature_content_after_audio))
+                .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_creature_answers))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_creature_tips_answers), getStringArray(R.array.game_task_creature_tips_messages))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_creature_tips_general_messages))
+                .getTask();
+    }
+
+    private GameTaskData createTaskFinal() {
+        return new GameTaskBuilder(99).withTitle(getString(R.string.game_task_final_title))
+                .withTextElement(getString(R.string.game_task_final_message))
+                .getTask();
+    }
+
+    private String getString(int resId)
+    {
+        return this.context.getString(resId);
+    }
+
+    private String[] getStringArray(int resId)
+    {
+        return this.context.getResources().getStringArray(resId);
+    }
+
+    private float getFloat(int resId) {
+        return this.context.getResources().getDimension(resId);
+    }
+}
