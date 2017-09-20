4754c58af02695922bb1c66113ece17456c94803
smoriak <smoriak@softserveinc.com>
2017-09-20 10:46:20 +0200

    test game - all tasks and pictures

--- testgame/src/main/java/com/slamcode/testgame/data/TestGameDataBundleProvider.java
+++ testgame/src/main/java/com/slamcode/testgame/data/TestGameDataBundleProvider.java
@@ -28,6 +28,9 @@ public final class TestGameDataBundleProvider implements GameDataBundleProvider<
         gameTasks.add(createTaskKiller());
         gameTasks.add(createTaskCreature());
         gameTasks.add(createTaskPlace());
+        gameTasks.add(createTaskDay());
+        gameTasks.add(createTaskMusic());
+        gameTasks.add(createTaskPoem());
 
         gameTasks.add(createTaskFinal());
 
@@ -46,6 +49,7 @@ public final class TestGameDataBundleProvider implements GameDataBundleProvider<
 
     private GameTaskData createTaskCat() {
         return new GameTaskBuilder(1).withTitle(getString(R.string.game_task_cat_title))
+                .withPictureElement(R.drawable.cats)
                 .withTextElement(getString(R.string.game_task_cat_content))
                 .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_cat_answers))
                 .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_cat_tips_answers), getStringArray(R.array.game_task_cat_tips_messages))
@@ -56,6 +60,7 @@ public final class TestGameDataBundleProvider implements GameDataBundleProvider<
     private GameTaskData createTaskTheatre()
     {
         return new GameTaskBuilder(2).withTitle(getString(R.string.game_task_theatre_title))
+                .withPictureElement(R.drawable.gaudi)
                 .withTextElement(getString(R.string.game_task_theatre_content))
                 .withLocationComparisonElement(getString(R.string.game_task_location_input_confirm_button_text),
                         getFloat(R.dimen.game_task_theatre_location_latitude),
@@ -75,6 +80,7 @@ public final class TestGameDataBundleProvider implements GameDataBundleProvider<
 
     private GameTaskData createTaskCreature() {
         return new GameTaskBuilder(4).withTitle(getString(R.string.game_task_creature_title))
+                .withPictureElement(R.drawable.budapest)
                 .withTextElement(getString(R.string.game_task_creature_content_before_audio))
                 .withAudioPlayerElement(R.raw.creature_2, null, this.context)
                 .withTextElement(getString(R.string.game_task_creature_content_after_audio))
@@ -84,21 +90,58 @@ public final class TestGameDataBundleProvider implements GameDataBundleProvider<
                 .getTask();
     }
 
-    private GameTaskData createTaskFinal() {
-        return new GameTaskBuilder(99).withTitle(getString(R.string.game_task_final_title))
-                .withTextElement(getString(R.string.game_task_final_message))
-                .getTask();
-    }
-
     private GameTaskData createTaskPlace()
     {
-        return new GameTaskBuilder(2).withTitle(getString(R.string.game_task_place_title))
+        return new GameTaskBuilder(5).withTitle(getString(R.string.game_task_place_title))
+                .withPictureElement(R.drawable.walk)
                 .withTextElement(getString(R.string.game_task_place_content))
                 .withLocationComparisonElement(getString(R.string.game_task_location_input_confirm_button_text),
                         getFloat(R.dimen.game_task_place_location_latitude),
                         getFloat(R.dimen.game_task_place_location_longitude),
                         getFloat(R.dimen.game_task_place_location_acceptance_distance),
                         null)
+                .getTask();
+    }
+
+    private GameTaskData createTaskDay() {
+        return new GameTaskBuilder(6).withTitle(getString(R.string.game_task_day_title))
+                .withPictureElement(R.drawable.valencia)
+                .withTextElement(getString(R.string.game_task_day_content))
+                .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_day_answers))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_day_tips_answers), getStringArray(R.array.game_task_day_tips_messages))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_day_tips_general_messages))
+                .getTask();
+    }
+
+    private GameTaskData createTaskMusic() {
+        return new GameTaskBuilder(7).withTitle(getString(R.string.game_task_music_title))
+                .withPictureElement(R.drawable.rojek)
+                .withTextElement(getString(R.string.game_task_music_before_audio_content))
+                .withTextElement(getString(R.string.game_task_music_link_content))
+                .withTextElement(getString(R.string.game_task_music_after_audio_content))
+                .withTextInputComparisonElement(getString(R.string.game_task_input_confirm_button_text), getStringArray(R.array.game_task_music_answers))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_music_tips_answers), getStringArray(R.array.game_task_music_tips_messages))
+                .withTipsForPreviousTextInputElement(getStringArray(R.array.game_task_music_tips_general_messages))
+                .getTask();
+    }
+
+    private GameTaskData createTaskPoem() {
+        return new GameTaskBuilder(8).withTitle(getString(R.string.game_task_poem_title))
+                .withPictureElement(R.drawable.mountains)
+                .withTextElement(getString(R.string.game_task_poem_content))
+                .withLocationComparisonElement(getString(R.string.game_task_location_input_confirm_button_text),
+                        getFloat(R.dimen.game_task_poem_location_latitude),
+                        getFloat(R.dimen.game_task_poem_location_longitude),
+                        getFloat(R.dimen.game_task_poem_location_acceptance_distance),
+                        null)
+                .getTask();
+    }
+
+    private GameTaskData createTaskFinal() {
+        return new GameTaskBuilder(99).withTitle(getString(R.string.game_task_final_title))
+                .withPictureElement(R.drawable.statue)
+                .withTextElement(getString(R.string.game_task_final_message))
+                .withPictureElement(R.drawable.flowers)
                 .getTask();
     }
 
